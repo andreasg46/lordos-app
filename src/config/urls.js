6b72cb6c38deb0a293c74c1b8c6eda8b52aaed60
Andreas Georgiou <andreasg.silversky@gmail.com>
2022-08-10 11:57:56 +0300

    Minor fixes

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,3 +1,3 @@
-export const api_server_url = (window.location.hostname === 'localhost') ? 'http://localhost:7050/api-lordos/' : 'http://dev.silversky3d.com/api-lordos/';
+export const api_server_url = (window.location.hostname === 'localhost') ? 'http://localhost:7050/api-lordos' : 'http://dev.silversky3d.com/api-lordos';
 export const app_url = (window.location.hostname === 'localhost') ? 'http://localhost:5000' : 'http://lordos.netlify.app';
 // export const PATH = (window.location.hostname === 'localhost') ? '/' : '/lordos-app/';
