8cdd06a740dd7fbe9f556ad1a07dd03c465db029
Andreas Georgiou <andreasg.silversky@gmail.com>
2022-08-19 12:55:58 +0300

    Testing

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,3 +1,3 @@
-export const phase_A_time = '13:05:00';
+export const phase_A_time = '13:15:00';
 export const phase_B_time = '14:15:00';
-export const phase_C_time = '15:30:00';
+export const phase_C_time = '15:15:00';
