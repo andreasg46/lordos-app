e858b61f733f8385d1ae6452cf49b0043de733a5
Andreas Georgiou <andreasg.silversky@gmail.com>
2022-08-23 18:36:00 +0300

    Test

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { findOtherUsers, findOtherUsersAnswered, findUserAnswered } from 'src/services/APIs';
 import { getCookie } from 'src/services/Cookies';
 import { GetCurrentDeadline, GetCurrentPhase, GetPendingPhase, GetPendingPhaseTime, GetPreviousPhase } from 'src/config/globals';
@@ -12,8 +12,13 @@ import { CBadge, CButton } from '@coreui/react-pro';
 import { getRandomInt, setupWindowHistoryTricks } from 'src/helpers';
 import CIcon from '@coreui/icons-react';
 import { cilPencil } from '@coreui/icons';
+import { useDispatch } from 'react-redux';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const stableDispatch = useCallback(dispatch, []);
+
+
   CheckSession();
 
   const [loader, setLoader] = useState(true);
@@ -55,10 +60,9 @@ const Home = () => {
 
   useEffect(() => {
     setupWindowHistoryTricks();
-
     GetPhase();
     GetOtherUsersResponses();
-  }, []);
+  }, [stableDispatch]);
 
   const GetPhase = () => {
     console.log("Getting Phase.. ");
